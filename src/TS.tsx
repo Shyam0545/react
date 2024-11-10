82fc57e33f8704ae5e5eaaae710d5f4be1de282a
shyamreddy <donthireddy.shyam@gmail.com>
2024-11-10 15:35:16 +0530

    commit changes

---
+++ src/TS.tsx
@@ -0,0 +1,104 @@
+import { useMemo } from 'react';
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+  type MRT_ColumnDef,
+} from 'material-react-table';
+import { data, type Person } from './makeData';
+import { useState } from "react";
+
+
+export function App() {
+
+  fetch("http://localhost:5555/apache_pinot")
+    .then((res) => res.json())
+    .then((json) => {
+      setData(json)
+    });
+
+}
+
+
+const Example = () => {
+  const columns = useMemo<MRT_ColumnDef<Person>[]>(
+    () => [
+      {
+        accessorKey: 'agent_id',
+        header: 'agent_id',
+        size: 200,
+      },
+      {
+        accessorKey: 'call_id',
+        header: 'call_id',
+        size: 200,
+      },
+      {
+        accessorKey: 'call_start',
+        header: 'call_start',
+        size: 200,
+      },
+      {
+        accessorKey: 'call_end',
+        header: 'call_end',
+        size: 200,
+      },
+      {
+        accessorKey: 'call_outcome',
+        header: 'call_outcome',
+        size: 200,
+      },
+      {
+        accessorKey: 'call_status',
+        header: 'call_status',
+        size: 200,
+      },
+      {
+        accessorKey: 'company_id',
+        header: 'company_id',
+        size: 200,
+      },
+      {
+        accessorKey: 'department_id',
+        header: 'department_id',
+        size: 200,
+      },
+    {
+        accessorKey: 'duration',
+        header: 'duration',
+        size: 200,
+    },
+        {
+            accessorKey: 'row_id',
+            header: 'row_id',
+            size: 200,
+        },
+        {
+            accessorKey: 'timestamp',
+            header: 'timestamp',
+            size: 200,
+        },
+    ],
+    [],
+  );
+
+  const table = useMaterialReactTable({
+    columns,
+    data,
+    initialState: { showColumnFilters: true },
+  });
+  return <MaterialReactTable table={table} />;
+};
+
+//Date Picker Imports - these should just be in your Context Provider
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+
+const ExampleWithLocalizationProvider = () => (
+  //App.tsx or AppProviders file
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <Example />
+  </LocalizationProvider>
+);
+
+export default ExampleWithLocalizationProvider;
+
